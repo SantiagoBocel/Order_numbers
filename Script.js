c47dcd2343814dd6919cba9f8977527844282630
Santiago Bocel <santiagobocel10@gmail.com>
2021-03-15 20:54:45 -0600

    Mostrar numeros

---
+++ Script.js
@@ -0,0 +1,8 @@
+const numbers = document.getElementById('numbers');
+const result = document.getElementById('result');
+
+let a = prompt('Introduzca el primer número');
+let b = prompt('Introduzca el segundo número');
+let c = prompt('Introduzca el tercer número');
+
+numbers.textContent =  'Los numeros introducidos son: '+a+' '+b+' '+c;
