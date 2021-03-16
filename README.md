241e217a72c215bffb0a46bfe6096e195fff1349
Santiago Bocel <santiagobocel10@gmail.com>
2021-03-16 13:55:23 -0600

    Ordenar numeros

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Order_numbers
+Este Proyecto realizado en JavaScript es un pequeño ejercicio de ordenamiento de números.
--- Script.js
+++ Script.js
@@ -5,4 +5,31 @@ let a = prompt('Introduzca el primer número');
 let b = prompt('Introduzca el segundo número');
 let c = prompt('Introduzca el tercer número');
 
-numbers.textContent =  'Los numeros introducidos son: '+a+' '+b+' '+c;
+numbers.textContent = 'Los numeros introducidos son: ' + a + ' ' + b + ' ' + c;
+
+//Validar el mayor
+if (a >= b && a >= c) {
+    if (b > c) {
+        result.textContent = "El orden es: " + a + " " + b + " " + c;
+    }
+    else {
+        result.textContent = "El orden es: " + a + " " + c + " " + b;
+    }
+}
+else if (b >= a && b >= c) {
+    if (a > c) {
+        result.textContent = "El orden es: " + b + " " + a + " " + c;
+    }
+    else {
+        result.textContent = "El orden es: " + b + " " + c + " " + a;
+    }
+}
+else if (c >= a && c >= b) {
+    if (a > b) {
+        result.textContent = "El orden es: " + c + " " + a + " " + b;
+    }
+    else {
+        result.textContent = "El orden es: " + c + " " + b + " " + a;
+    }
+
+}
